fa942fa14ddd6932addfe0f9f513d021ac747305
Tuwuh S <tuwuhs@gmail.com>
2015-01-17 04:44:12 +0000

    Create image-scroller application

---
+++ image-scroller.cc
@@ -0,0 +1,302 @@
+// -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
+//
+// This code is public domain
+// (but note, that the led-matrix library this depends on is GPL v2)
+
+#include "led-matrix.h"
+#include "threaded-canvas-manipulator.h"
+
+#include <assert.h>
+#include <getopt.h>
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <algorithm>
+
+using std::min;
+using std::max;
+
+using namespace rgb_matrix;
+
+class ImageScroller : public ThreadedCanvasManipulator {
+public:
+  // Scroll image with "scroll_jumps" pixels every "scroll_ms" milliseconds.
+  // If "scroll_ms" is negative, don't do any scrolling.
+  ImageScroller(Canvas *m, int scroll_jumps, int scroll_ms = 30)
+    : ThreadedCanvasManipulator(m), scroll_jumps_(scroll_jumps),
+      scroll_ms_(scroll_ms),
+      horizontal_position_(0) {
+  }
+
+  virtual ~ImageScroller() {
+    Stop();
+    WaitStopped();   // only now it is safe to delete our instance variables.
+  }
+
+  // _very_ simplified. Can only read binary P6 PPM. Expects newlines in headers
+  // Not really robust. Use at your own risk :)
+  // This allows reload of an image while things are running, e.g. you can
+  // life-update the content.
+  bool LoadPPM(const char *filename) {
+    FILE *f = fopen(filename, "r");
+    if (f == NULL) return false;
+    char header_buf[256];
+    const char *line = ReadLine(f, header_buf, sizeof(header_buf));
+#define EXIT_WITH_MSG(m) { fprintf(stderr, "%s: %s |%s", filename, m, line); \
+      fclose(f); return false; }
+    if (sscanf(line, "P6 ") == EOF)
+      EXIT_WITH_MSG("Can only handle P6 as PPM type.");
+    line = ReadLine(f, header_buf, sizeof(header_buf));
+    int new_width, new_height;
+    if (!line || sscanf(line, "%d %d ", &new_width, &new_height) != 2)
+      EXIT_WITH_MSG("Width/height expected");
+    int value;
+    line = ReadLine(f, header_buf, sizeof(header_buf));
+    if (!line || sscanf(line, "%d ", &value) != 1 || value != 255)
+      EXIT_WITH_MSG("Only 255 for maxval allowed.");
+    const size_t pixel_count = new_width * new_height;
+    Pixel *new_image = new Pixel [ pixel_count ];
+    assert(sizeof(Pixel) == 3);   // we make that assumption.
+    if (fread(new_image, sizeof(Pixel), pixel_count, f) != pixel_count) {
+      line = "";
+      EXIT_WITH_MSG("Not enough pixels read.");
+    }
+#undef EXIT_WITH_MSG
+    fclose(f);
+    fprintf(stderr, "Read image '%s' with %dx%d\n", filename,
+            new_width, new_height);
+    horizontal_position_ = 0;
+    MutexLock l(&mutex_new_image_);
+    new_image_.Delete();  // in case we reload faster than is picked up
+    new_image_.image = new_image;
+    new_image_.width = new_width;
+    new_image_.height = new_height;
+    return true;
+  }
+
+  void Run() {
+    const int screen_height = canvas()->height();
+    const int screen_width = canvas()->width();
+    while (running()) {
+      {
+        MutexLock l(&mutex_new_image_);
+        if (new_image_.IsValid()) {
+          current_image_.Delete();
+          current_image_ = new_image_;
+          new_image_.Reset();
+        }
+      }
+      if (!current_image_.IsValid()) {
+        usleep(100 * 1000);
+        continue;
+      }
+      for (int x = 0; x < screen_width; ++x) {
+        for (int y = 0; y < screen_height; ++y) {
+          const Pixel &p = current_image_.getPixel(
+                     (horizontal_position_ + x) % current_image_.width, y);
+          canvas()->SetPixel(x, y, p.red, p.green, p.blue);
+        }
+      }
+      horizontal_position_ += scroll_jumps_;
+      if (horizontal_position_ < 0) horizontal_position_ = current_image_.width;
+      if (scroll_ms_ <= 0) {
+        // No scrolling. We don't need the image anymore.
+        current_image_.Delete();
+      } else {
+        usleep(scroll_ms_ * 1000);
+      }
+    }
+  }
+
+private:
+  struct Pixel {
+    Pixel() : red(0), green(0), blue(0){}
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+  };
+
+  struct Image {
+    Image() : width(-1), height(-1), image(NULL) {}
+    ~Image() { Delete(); }
+    void Delete() { delete [] image; Reset(); }
+    void Reset() { image = NULL; width = -1; height = -1; }
+    inline bool IsValid() { return image && height > 0 && width > 0; }
+    const Pixel &getPixel(int x, int y) {
+      static Pixel black;
+      if (x < 0 || x >= width || y < 0 || y >= height) return black;
+      return image[x + width * y];
+    }
+
+    int width;
+    int height;
+    Pixel *image;
+  };
+
+  // Read line, skip comments.
+  char *ReadLine(FILE *f, char *buffer, size_t len) {
+    char *result;
+    do {
+      result = fgets(buffer, len, f);
+    } while (result != NULL && result[0] == '#');
+    return result;
+  }
+
+  const int scroll_jumps_;
+  const int scroll_ms_;
+
+  // Current image is only manipulated in our thread.
+  Image current_image_;
+
+  // New image can be loaded from another thread, then taken over in main thread.
+  Mutex mutex_new_image_;
+  Image new_image_;
+
+  int32_t horizontal_position_;
+};
+
+static int usage(const char *progname) {
+  fprintf(stderr, "usage: %s <options> -D <demo-nr> [optional parameter]\n",
+          progname);
+  fprintf(stderr, "Options:\n"
+          "\t-r <rows>     : Display rows. 16 for 16x32, 32 for 32x32. "
+          "Default: 32\n"
+          "\t-c <chained>  : Daisy-chained boards. Default: 1.\n"
+          "\t-L            : 'Large' display, composed out of 4 times 32x32\n"
+          "\t-p <pwm-bits> : Bits used for PWM. Something between 1..11\n"
+          "\t-l            : Don't do luminance correction (CIE1931)\n"
+          "\t-D <demo-nr>  : Always needs to be set\n"
+          "\t-d            : run as daemon. Use this when starting in\n"
+          "\t                /etc/init.d, but also when running without\n"
+          "\t                terminal (e.g. cron).\n"
+          "\t-t <seconds>  : Run for these number of seconds, then exit.\n"
+          "\t       (if neither -d nor -t are supplied, waits for <RETURN>)\n");
+  fprintf(stderr, "Demos, choosen with -D\n");
+  fprintf(stderr, "\t0  - some rotating square\n"
+          "\t1  - forward scrolling an image (-m <scroll-ms>)\n"
+          "\t2  - backward scrolling an image (-m <scroll-ms>)\n"
+          "\t3  - test image: a square\n"
+          "\t4  - Pulsing color\n"
+          "\t5  - Grayscale Block\n"
+          "\t6  - Abelian sandpile model (-m <time-step-ms>)\n"
+          "\t7  - Conway's game of life (-m <time-step-ms>)\n"
+          "\t8  - Langton's ant (-m <time-step-ms>)\n"
+          "\t9  - Volume bars (-m <time-step-ms>)\n");
+  fprintf(stderr, "Example:\n\t%s -t 10 -D 1 runtext.ppm\n"
+          "Scrolls the runtext for 10 seconds\n", progname);
+  return 1;
+}
+
+int main(int argc, char *argv[]) {
+  bool as_daemon = false;
+  int rows = 16;
+  int chain = 4;
+  int scroll_ms = 30;
+  int pwm_bits = 8;
+  bool do_luminance_correct = true;
+
+  const char *image_filename = "runtext16.ppm";
+
+  int opt;
+  while ((opt = getopt(argc, argv, "dlD:t:r:p:c:m:L")) != -1) {
+    switch (opt) {
+    case 'd':
+      as_daemon = true;
+      break;
+
+    case 'r':
+      rows = atoi(optarg);
+      break;
+
+    case 'c':
+      chain = atoi(optarg);
+      break;
+
+    case 'm':
+      scroll_ms = atoi(optarg);
+      break;
+
+    case 'p':
+      pwm_bits = atoi(optarg);
+      break;
+
+    case 'l':
+      do_luminance_correct = !do_luminance_correct;
+      break;
+
+    default: /* '?' */
+      return usage(argv[0]);
+    }
+  }
+  
+  if (optind < argc) {
+    image_filename = argv[optind];
+  }
+  
+  if (getuid() != 0) {
+    fprintf(stderr, "Must run as root to be able to access /dev/mem\n"
+            "Prepend 'sudo' to the command:\n\tsudo %s ...\n", argv[0]);
+    return 1;
+  }
+
+  // Initialize GPIO pins. This might fail when we don't have permissions.
+  GPIO io;
+  if (!io.Init())
+    return 1;
+
+  // Start daemon before we start any threads.
+  if (as_daemon) {
+    if (fork() != 0)
+      return 0;
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+  }
+
+  // The matrix, our 'frame buffer' and display updater.
+  RGBMatrix *matrix = new RGBMatrix(&io, rows, chain);
+  matrix->set_luminance_correct(do_luminance_correct);
+  if (pwm_bits >= 0 && !matrix->SetPWMBits(pwm_bits)) {
+    fprintf(stderr, "Invalid range of pwm-bits\n");
+    return 1;
+  }
+
+  Canvas *canvas = matrix;
+
+  // The ThreadedCanvasManipulator objects are filling
+  // the matrix continuously.
+  ThreadedCanvasManipulator *image_gen = NULL;
+  if (image_filename) {
+    ImageScroller *scroller = new ImageScroller(canvas, 1, scroll_ms);
+    if (!scroller->LoadPPM(image_filename))
+      return 1;
+    image_gen = scroller;
+  }
+
+  if (image_gen == NULL)
+    return usage(argv[0]);
+
+  // Image generating demo is crated. Now start the thread.
+  image_gen->Start();
+
+  // Now, the image genreation runs in the background. We can do arbitrary
+  // things here in parallel. In this demo, we're essentially just
+  // waiting for one of the conditions to exit.
+  if (as_daemon) {
+    sleep(INT_MAX);
+  } else {
+    // Things are set up. Just wait for <RETURN> to be pressed.
+    printf("Press <RETURN> to exit and reset LEDs\n");
+    getchar();
+  }
+
+  // Stop image generating thread.
+  delete image_gen;
+  delete canvas;
+
+  return 0;
+}
